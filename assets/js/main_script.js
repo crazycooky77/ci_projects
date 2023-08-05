8039d100ad355e96cb370e255f8d7bc250f20f20
Yasmin Escaros-Buechsel <yescaros@apple.com>
2023-08-05 17:20:29 +0100

    Revert "fix(script): testing fix for stuck focus on iOS Safari"
    This reverts commit 78a61c307400311325cd41f36aa192c6874724fa.

--- assets/js/main_script.js
+++ assets/js/main_script.js
@@ -63,5 +63,3 @@ $(document).on('keydown', function(event) {
         document.activeElement.blur();
     }
 });
-
-(function(l){var i,s={touchend:function(){}};for(i in s)l.addEventListener(i,s)})(document); // sticky hover fix in iOS
