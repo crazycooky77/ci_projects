52facff855ac0672e2cdd0cdf722f9662ba3ce85
Yasmin Escaros-Buechsel <yescaros@apple.com>
2023-08-30 19:36:08 +0100

    Add division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,8 @@ function runGame(gameType) {
 		displaySubtractQuestion(num1, num2);
 	} else if (gameType === 'multiply') {
 		displayMultiplyQuestion(num1, num2);
+	} else if (gameType === 'division') {
+		displayDivisionQuestion(num1, num2);
 	} else {
 		alert(`Unknown game type: ${gameType}`);
 		throw `Unknown game type: ${gameType}. Aborting!`;
@@ -68,6 +70,8 @@ function calculateCorrectAnswer() {
 		return [operand1 - operand2, 'subtract'];
 	} else if (operator === 'x') {
 		return [operand1 * operand2, 'multiply'];
+	} else if (operator === '/') {
+		return [operand1 / operand2, 'division'];
 	} else {
 		alert(`Unimplemented operator ${operator}`);
 		throw `Unimplemented operator ${operator}. Aborting!`;
@@ -106,4 +110,10 @@ function displayMultiplyQuestion(operand1, operand2) {
 	document.getElementById('operand1').textContent = operand1;
 	document.getElementById('operand2').textContent = operand2;
 	document.getElementById('operator').textContent = 'x';
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+	document.getElementById('operand1').textContent = operand1;
+	document.getElementById('operand2').textContent = operand2;
+	document.getElementById('operator').textContent = '/';
 }
