3d46d6054324591950f4fab459c1d8dff80b6788
Yasmin Escaros-Buechsel <yescaros@apple.com>
2023-09-08 17:59:38 +0100

    feat(script): adding script to stop the game when the user loses

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,27 +57,44 @@ function letterSelection(event) {
 	let clickedLetter = event.className;
 	let blankMatch = document.getElementById('puzzle').getElementsByClassName(clickedLetter);
 // Disable the clicked alphabet button
-	let clickedButton = document.getElementById('alphabet').getElementsByClassName(clickedLetter)[0]
-	clickedButton.disabled = true
-// Get the letter clicked in the alphabet and replace it with the strikethrough version
-	let clickedAlphabet = document.getElementById('alphabet').getElementsByClassName(clickedLetter)[0].getElementsByTagName('img');
-	let letterStrikethrough = clickedAlphabet[0].src = 'assets/images/' + clickedLetter + '-selected.png'
-// Change the cursor to default for the clicked letter
-	clickedAlphabet[0].style.cursor = 'default'
+	let clickedButton = document.getElementById('alphabet').getElementsByClassName(clickedLetter)[0];
+// Get the letter clicked in the alphabet
+	let clickedAlphabet = document.getElementById('alphabet').getElementsByClassName(clickedLetter)[0].getElementsByTagName('img');	
+// Get the stage of the hangman image and set various variables
+	let hangman = document.getElementById('hangman-image').getElementsByTagName('img');
+	let hangmanAttr = hangman[0].getAttribute('src');
+	let hangmanSrc = hangmanAttr.substr(0, 22);
+	let hangmanNr = Number(hangmanAttr.substr(22, 1));
 
+// Function to disable the clicked letter, replace the image with a strikethrough image, and set the cursor style to default
+	function clickActions() {
+		clickedButton.disabled = true;
+		let letterStrikethrough = clickedAlphabet[0].src = 'assets/images/' + clickedLetter + '-selected.png';
+		clickedAlphabet[0].style.cursor = 'default';
+	}
+
+	if (blankMatch.length > 0 && hangmanNr < 8) {
+// Run functions to update the clicked button
+		clickActions()
 // Replace all relevant blank puzzle letters with the selected alphabet letter
-	if (blankMatch.length > 0) {
 		for (let i = 0; i < blankMatch.length; i++) {
-			let replaceLetter = blankMatch[i].src = 'assets/images/' + clickedLetter + '.png'
+			let replaceLetter = blankMatch[i].src = 'assets/images/' + clickedLetter + '.png';
 			blankMatch[i].style.marginTop = '-44px';
 		}
-// TODO: If statement to check for the last hangman image (8) and "Game over" if reached		
-	} else {
+	} else if (blankMatch.length === 0 && hangmanNr < 7) {
+// Run functions to update the clicked button
+		clickActions()
 // Replace the hangman image for every wrong answer
-			let hangman = document.getElementById('hangman-image').getElementsByTagName('img');
-			hangmanAttr = hangman[0].getAttribute('src')
-			hangmanSrc = hangmanAttr.substr(0, 22)
-			hangmanNr = hangmanAttr.substr(22, 1)
-			let hangmanHung = hangman[0].src = hangmanSrc + (Number(hangmanNr) + 1) + '.png'
+		let hangmanHung = hangman[0].src = hangmanSrc + (hangmanNr + 1) + '.png';
+	} else if (blankMatch.length === 0 && hangmanNr === 7) {
+// Run functions to update the clicked button
+		clickActions()
+// Replace the hangman image for the wrong answer and provide Game Over message
+		let hangmanHung = hangman[0].src = hangmanSrc + (hangmanNr + 1) + '.png';
+		setTimeout(() => {
+			alert('Game Over');}, 200);
+// If the user keeps trying to play, ask them to start a new game
+	} else {
+		alert('Please start a new game');
 	}
 }
